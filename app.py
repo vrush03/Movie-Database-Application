15c794da45a3f4cd1974535aea426d526e65a635
jiteshjha <jiteshjha96@gmail.com>
2016-03-07 18:24:26 +0530

    MySQL cursor and sign up implementation

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 
 from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+
+#MySQL configurations
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'remo161196'
+app.config['MYSQL_DATABASE_DB'] = 'movieapp'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 # route to index.html
 @app.route("/")
@@ -15,17 +28,35 @@ def showSignUp():
 # interact with MySQL for sign up
 @app.route('/signUp',methods=['POST'])
 def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
-    # read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
+        # validate the received values
+        if _name and _email and _password:
 
-    # validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+            # All Good, let's call MySQL
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close()
+        conn.close()
 
 if __name__ == "__main__":
     app.run()
