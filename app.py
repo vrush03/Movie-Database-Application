9a4f9d3bf997a52e568bdc277d4c46fc8ab0a145
jiteshjha <jiteshjha96@gmail.com>
2016-03-31 00:45:47 +0530

    Logout logic

--- app.py
+++ app.py
@@ -96,5 +96,10 @@ def userHome():
     else:
         return render_template('error.html',error = 'Unauthorized Access')
 
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+
 if __name__ == "__main__":
     app.run()
